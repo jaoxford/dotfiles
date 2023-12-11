7b6bc6b0e732df0eb768b4cfbedcc4de96d26b67
Jacob Oxford <jacoboxford12345@gmail.com>
2023-12-11 02:21:31 +0000

    Rewrite script to read data from extensions_list.txt
    Meaning only 2 scripts are needed to manage vscodium extensions.

--- vscodium/install_codium_extensions.sh
+++ vscodium/install_codium_extensions.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
 # This script installs a list of Visual Studio Code (VSCodium) extensions.
-# The packagelist is the output from "codium --list-extensions"
-extensions=(
-    bradlc.vscode-tailwindcss
-    dbaeumer.vscode-eslint
-    dracula-theme.theme-dracula
-    esbenp.prettier-vscode
-    formulahendry.auto-close-tag
-    formulahendry.auto-rename-tag
-    mechatroner.rainbow-csv
-    ms-python.python
-    oderwat.indent-rainbow
-    PKief.material-icon-theme
-)
+# Extensions are read from "extensions_list.txt"
 
-for extension in "${extensions[@]}"; do
+while IFS= read -r extension; do
     codium --install-extension "$extension"
-done
+done < vscodium/extensions_list.txt
