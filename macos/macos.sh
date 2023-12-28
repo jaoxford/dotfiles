d52b582fd0005b41e01c2a00e911cb14d10dfdae
Jacob Oxford <jacoboxford12345@gmail.com>
2023-12-28 00:46:13 +0000

    Keyboard repeat was too much. Sensible to use default values instead.

--- macos/macos.sh
+++ macos/macos.sh
@@ -36,10 +36,6 @@ defaults write com.apple.finder FXDefaultSearchScope -bool "true"
 # Finder: show status bar
 defaults write com.apple.finder ShowStatusBar -bool "true"
 
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
-
 ###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
