5e30cfbdab90435719a34e13be38cb853d2f97c3
Jacob Oxford <jacoboxford12345@gmail.com>
2024-01-18 16:49:17 +0000

    Adding comments to script.

--- git/hooks/branch_name_in_commit_message.sh
+++ git/hooks/branch_name_in_commit_message.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-# prepending commit message.
+# This script prepends the branch name to commit messages in Git.
+# It skips this action for specific branches like master, develop, and release.
+
+# Define branches to skip. If not already defined, use default values.
 if [ -z "$BRANCHES_TO_SKIP" ]; then
   BRANCHES_TO_SKIP=(master develop release)
 fi
 
+# Get the current branch name
 BRANCH_NAME=$(git symbolic-ref --short HEAD)
+# Strip any leading directory components (in case the branch is in a folder)
 BRANCH_NAME="${BRANCH_NAME##*/}"
 
+# Check if the current branch is in the list of branches to skip
 BRANCH_EXCLUDED=$(printf "%s\n" "${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
+# Check if the commit message already contains the branch name
 BRANCH_IN_COMMIT=$(grep -c "\[$BRANCH_NAME\]" $1)
 
+# If on a relevant branch and the branch name is not already in the commit message,
+# prepend the branch name to the commit message.
 if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then
+  # Prepend the branch name to the commit message
   sed -i.bak -e "1s/^/$BRANCH_NAME - /" $1
 fi
 
-# To run this you'll need to copy this into your prepare-commit-msg hook in .git/hooks
-# And then chmod +x prepare-commit-msg
+# Note:
+# To use this script, copy it into the 'prepare-commit-msg' hook in the .git/hooks directory of your repository.
+# Don't forget to make the script executable with 'chmod +x prepare-commit-msg'.
