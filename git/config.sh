5d6966ec06c38985a5aa8908dc7644e3b5d61338
Jacob Oxford <jacoboxford12345@gmail.com>
2024-02-16 00:36:44 +0000

    Git config for things like rerere, sorting branches, signing commits.
    Signed-off-by: Jacob Oxford <jacoboxford12345@gmail.com>

---
+++ git/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Tries to automatically apply same changes done during rebase / cherry pick
+git config --global rerere.enabled true
+# Sort branches by latest commits at top
+git config --global branch.sort -comitterdate
+# Sign commits by SSH rather than GPG
+git config gpg.format ssh
+# Location of SSH key for signing
+git config user.signingkey ~/.ssh/id_ed25519.pub
