cfa5c0ecce76fca46ff70b01ffb4805e9a366451
Jacob Oxford <jacoboxford12345@gmail.com>
2024-03-22 00:15:11 +0000

    Sort git branches by columns.

--- git/config.sh
+++ git/config.sh
@@ -4,6 +4,8 @@ git config --global user.email "jacoboxford12345@gmail.com"
 
 # Tries to automatically apply same changes done during rebase / cherry pick
 git config --global rerere.enabled true
+# Sort branches into columns
+git config --global column.ui auto
 # Sort branches by latest commits at top
 git config --global branch.sort -committerdate
 # Sign commits by SSH rather than GPG
