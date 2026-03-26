5c3f870fe84a1fe7b6d5e2c95b0c3d16a4baef70
Jacob Oxford <jacoboxford12345@gmail.com>
2026-03-26 14:52:53 +0000

    Now using Zed as general text editor. VSCodium is gone.

--- vscodium/install_codium_extensions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script installs a list of Visual Studio Code (VSCodium) extensions.
-# Extensions are read from "extensions_list.txt"
-
-while IFS= read -r extension; do
-    codium --install-extension "$extension"
-done < vscodium/extensions_list.txt
--- vscodium/list_codium_extensions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Captures the list of currently installed extensions and writes them to a file
-codium --list-extensions > vscodium/extensions_list.txt
