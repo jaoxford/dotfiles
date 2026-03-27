67b867803bd19cf0e6d4f736aeba53657cb70bbb
Jacob Oxford <jacoboxford12345@gmail.com>
2026-03-27 17:54:20 +0000

    Putting user linkers above XDG config home linkers.

--- omarchy/link.sh
+++ omarchy/link.sh
@@ -7,8 +7,10 @@ DOTFILES_DIR=$( cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd )
 CONFIG_DIR="${XDG_CONFIG_HOME}"
 
 echo "Linking config files..."
+
+ln -sf "$DOTFILES_DIR/git/global_gitignore" "$HOME/.gitignore"
+
 ln -sf "$SCRIPT_DIR/.config/hypr/bindings.conf" "$CONFIG_DIR/hypr/bindings.conf"
 ln -sf "$SCRIPT_DIR/.config/hypr/input.conf" "$CONFIG_DIR/hypr/input.conf"
 ln -sf "$SCRIPT_DIR/.config/hypr/monitors.conf" "$CONFIG_DIR/hypr/monitors.conf"
 ln -sf "$SCRIPT_DIR/.config/uwsm/default" "$CONFIG_DIR/uwsm/default"
-ln -sf "$DOTFILES_DIR/git/global_gitignore" "$HOME/.gitignore"
