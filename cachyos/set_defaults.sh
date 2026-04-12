b0fd3b93baa5559ed57f0a68d2d3d2ac03ff2b7a
Jacob Oxford <jacoboxford12345@gmail.com>
2026-04-13 00:40:57 +0100

    Turning on night light in CachyOs.

--- cachyos/set_defaults.sh
+++ cachyos/set_defaults.sh
@@ -4,3 +4,7 @@ xdg-settings set default-web-browser brave-browser.desktop
 
 # Do not want this to run on startup
 rm -f "$HOME/.config/autostart/cachyos-hello.desktop"
+
+# Want to cut down on blue lights at night
+# Changes may not apply until next login
+kwriteconfig6 --file kwinrc --group NightColor --key Active true
