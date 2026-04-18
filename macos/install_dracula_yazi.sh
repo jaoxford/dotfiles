6e7ab0879dc5ac08d287eedc4b6f0b539ba4658c
Jacob Oxford <jacoboxford12345@gmail.com>
2026-04-18 14:52:39 +0100

    Better to install dracula on yazi via their package manager.

--- macos/install_dracula_yazi.sh
+++ macos/install_dracula_yazi.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-YAZI_DIR="$HOME/.config/yazi/flavors"
-if [ ! -d "$YAZI_DIR" ]; then
-    git clone https://github.com/dracula/yazi.git "$HOME/.config/yazi/flavors/dracula.yazi"
-fi
-
+ya pkg add yazi-rs/flavors:dracula
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ln -sf "$SCRIPT_DIR/.config/yazi/yazi.toml" "$HOME/.config/yazi/yazi.toml"
 ln -sf "$SCRIPT_DIR/.config/yazi/theme.toml" "$HOME/.config/yazi/theme.toml"
