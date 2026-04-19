65d9130402d3f7062597cdf813dab9b7da23d06f
Jacob Oxford <jacoboxford12345@gmail.com>
2026-04-19 12:19:30 +0100

    Moving install dracula yazi script to central location.

--- macos/install_dracula_yazi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ya pkg add yazi-rs/flavors:dracula
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ln -sf "$SCRIPT_DIR/.config/yazi/yazi.toml" "$HOME/.config/yazi/yazi.toml"
-ln -sf "$SCRIPT_DIR/.config/yazi/theme.toml" "$HOME/.config/yazi/theme.toml"
---
+++ yazi/install_dracula_yazi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ya pkg add yazi-rs/flavors:dracula
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DOTFILES_DIR=$( cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd )
+ln -sf "$DOTFILES_DIR/yazi/theme.toml" "$HOME/.config/yazi/theme.toml"
