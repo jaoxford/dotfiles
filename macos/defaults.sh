2ee6caa36e065b34081d5bdca16c5df2438992d8
Jacob Oxford <jacoboxford12345@gmail.com>
2026-05-05 16:42:59 +0100

    Disable window tinting macos.

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -14,6 +14,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Disable wallpaper tinting in windows
+defaults write NSGlobalDomain AppleReduceDesktopTinting -bool true
+
 # Always show scrollbars
 defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
 
